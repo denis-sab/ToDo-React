ee9c35c0285809a3d35668757295e56fbd670151
YagoPazos <yagopb@gmail.com>
2024-04-22 15:40:07 +0200

    updated Todo cmpnt

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
 import './Todo.css'
 
 function Todo() {
-    return <div className='m-3 d-flex justify-content-around'>
+    return <div className='m-3 d-flex justify-content-evenly'>
         <li className='list-group-item'>This is one LI item</li>
-        <button className='rounded card'><img className='iconssize rounded card' src="src\assets\edit.svg" alt="editbtn" /></button>
-        <button className='rounded card'><img className='iconssize rounded card' src="src/assets/bin.svg" alt="deletebtn" /></button>
+        <button className='rounded card'><img className='iconssize' src="src\assets\edit.svg" alt="editbtn" /></button>
+        <button className='rounded card'><img className='iconssize' src="src/assets/bin.svg" alt="deletebtn" /></button>
     </div>
 }
 
