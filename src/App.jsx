5870d613e8d84695c25bba08743e79dee9e367ce
YagoPazos <yagopb@gmail.com>
2024-04-22 16:10:27 +0200

    new buttons for Todo compnt

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ function App() {
   const [count, setCount] = useState(0)
   return <> 
           <Todo/>
-          <Todo/>
-          <Todo/>
   </>
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,9 +2,9 @@ import './Todo.css'
 
 function Todo() {
     return <div className='m-3 d-flex justify-content-evenly'>
-        <li className='list-group-item'>This is one LI item</li>
-        <button className='rounded card'><img className='iconssize' src="src\assets\edit.svg" alt="editbtn" /></button>
-        <button className='rounded card'><img className='iconssize' src="src/assets/bin.svg" alt="deletebtn" /></button>
+        <li className='list-group-item card fontstyle'>This is a LI element</li>
+        <button className='btn btn-outline-warning'><img className='iconssize' src="src\assets\edit.svg" alt="editbtn" /></button>
+        <button className='btn btn-outline-danger'><img className='iconssize' src="src/assets/bin.svg" alt="deletebtn" /></button>
     </div>
 }
 
