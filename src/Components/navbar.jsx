20381c80b1981058e2665a2eab14287d7dd4f7b9
YagoPazos <yagopb@gmail.com>
2024-04-23 17:22:46 +0200

    updated

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,7 +1,8 @@
 
 
 function Navbar () {
-    return <nav class="p-2 col logo d-flex justify-content-center"><img class="img-fluid" style="height: 150px;width: 150px;" src="./Assets/Imgs/notes-logo-transparent.png" alt="logo"></div>
+    return <nav class="">
+
         
     </nav>
 }
