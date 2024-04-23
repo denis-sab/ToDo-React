f96a57a15878727a958cee1ec1bce76459d2ff6d
Denis Sab <denis.sabs@ymail.com>
2024-04-23 18:19:46 +0200

    fixin main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Todo from "./Components/Todo"
-import Todolist from "./Components/Todolist"
-import AddItem from "./Components/AddItem"
+import Todo from "./components/Todo"
+import Todolist from "./components/Todolist"
+import AddItem from "./components/AddItem"
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
