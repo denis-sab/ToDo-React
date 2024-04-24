c6e987904678a0a541afb649f7f9659900b53773
YagoPazos <yagopb@gmail.com>
2024-04-24 17:07:26 +0200

    added workable style2

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,25 @@ import "./App.css";
 import './components/Todo.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/navbar';
+import Header from "./components/header";
 
 
 function App() {
   const [newTodo, setNewTodo] = useState({});
-
+  
+  const onChange = ({target}) => {
+    const {name, value} = target;
+    setNewTodo((prev) => prev({...prev, id:Date.now(), [name]: value}))
+  }
+  
   const [allTodos, setAllTodos] = useState([]);
+    const onSubmit = (event) => {
+      event.preventDefault();
+      if (!newTask.title) return;
+      setAllTodos((prev) => [newTodo, ...prev]);
+      setNewTodo({});
+  };
+
 
   const [localStorage, setLocalStorage] = useState();
 
@@ -21,15 +34,14 @@ function App() {
       
       <Navbar />
       
-      <header>
-        
-      </header>
+      <Header />
+
       <section className="cont d-flex justify-content-center">
-        <div className="col m-5 contclr">
-            <AddItem />
+        <div className="col m-5 contclr border border-primary rounded">
+            <AddItem onChange={onChange}/>
             
         </div>    
-        <div className="col m-5 contclr">
+        <div className="col m-5 contclr border border-primary rounded">
             <Todolist />
         </div>
       </section>
---
+++ src/Components/header.jsx
@@ -0,0 +1,11 @@
+import './Todo.css'
+
+function Header() {
+    return <div className='header p-5 d-flex justify-content-center align'>
+
+        <h1 className='p-4 text-center fontstyle font-weight-bold'>Making notes taste better!</h1>
+
+    </div>
+}
+
+export default Header;
