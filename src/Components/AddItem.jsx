c58fbd04cc0982c41cf88da914e325713bc09823
Denis Sab <denis.sabs@ymail.com>
2024-04-26 17:33:49 +0200

    Add Item after Maria recap

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 
 function AddnewTask() {
-
   //  useState
-  const [task, setTask] = useState(["Yago: espresso machen, doppio!", "Anita: Urlaub machen !", "Denis, local storage machen!"]);
+  const [task, setTask] = useState([
+    "Yago: espresso machen, doppio!",
+    "Anita: Urlaub machen !",
+    "Denis, local storage machen!",
+  ]);
   const [newTask, setNewTask] = useState("");
 
   // handleOnchange
@@ -11,12 +14,15 @@ function AddnewTask() {
     setNewTask(event.target.value);
   }
   // add Item function
-  function addTask() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
     if (newTask.trim() !== "") {
+      localStorage.setItem("item", JSON.stringify([...task, newTask]));
       setTask((t) => [...t, newTask]);
       setNewTask("");
     }
-  }
+  };
   // delet Item function
   function deleteTask(index) {
     const updatedTask = task.filter((_, i) => i !== index);
@@ -24,43 +30,53 @@ function AddnewTask() {
   }
 
   return (
-
     <div className="container bg-warning-subtle w-50   p-3 ">
+      <h4 className="bg-danger-subtle form-control border-success w-50 m-auto mb-2 ">
+        {" "}
+        Journal !
+      </h4>
 
-      <h4 className="bg-danger-subtle form-control border-success w-50 m-auto mb-2 "> Journal !</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor=""></label>
 
-      <label htmlFor=""></label>
+        <textarea
+          name=""
+          id=""
+          cols="50"
+          rows="5"
+          placeholder="Enter task..."
+          value={newTask}
+          onChange={handleInput}
+        ></textarea>
 
-      <textarea
+        <br />
 
-        name=""
-        id=""
-        cols="50"
-        rows="5"
-        placeholder="Enter task..."
-        value={newTask}
-        onChange={handleInput} >  
-        
-        </textarea>
-
-      <br />
-
-      <button
-        className="bg-danger-subtle border-warning border-opacity-75 "
-        type="submit"
-        onClick={addTask} >
+        <button
+          className="bg-danger-subtle border-warning border-opacity-75 "
+          type="submit"
           
-        Add new task </button>
+        >
+          Add new task{" "}
+        </button>
+      </form>
 
       <ol>
         {task.map((task, index) => (
           <li key={index}>
             {task}
-            <button onClick={() => deleteTask(index)} className='btn btn-outline-danger'><img className='iconssize' src="src/assets/bin.svg" alt="deletebtn" />  </button>
+            <button
+              onClick={() => deleteTask(index)}
+              className="btn btn-outline-danger"
+            >
+              <img
+                className="iconssize"
+                src="src/assets/bin.svg"
+                alt="deletebtn"
+              />{" "}
+            </button>
           </li>
         ))}
       </ol>
-
     </div>
   );
 }
