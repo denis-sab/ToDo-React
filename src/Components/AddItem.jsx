aa81af03d978c8038fce74773d8df3d30350af77
Denis Sab <denis.sabs@ymail.com>
2024-04-26 15:41:46 +0200

    adding some functionality

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -1,33 +1,68 @@
+import { useState } from "react";
 
+function AddnewTask() {
 
-function AddnewTask () {
+  //  useState
+  const [task, setTask] = useState(["Yago: espresso machen, doppio!", "Anita: Urlaub machen !", "Denis, local storage machen!"]);
+  const [newTask, setNewTask] = useState("");
 
-    return (
+  // handleOnchange
+  function handleInput(event) {
+    setNewTask(event.target.value);
+  }
+  // add Item function
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTask((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+  // delet Item function
+  function deleteTask(index) {
+    const updatedTask = task.filter((_, i) => i !== index);
+    setTask(updatedTask);
+  }
 
-    <div className='container bg-warning-subtle w-50   p-3 '>
+  return (
 
+    <div className="container bg-warning-subtle w-50   p-3 ">
 
- <h4 className="bg-danger-subtle form-control border-success w-50 m-auto mb-2 " >Add new task</h4>
+      <h4 className="bg-danger-subtle form-control border-success w-50 m-auto mb-2 "> Journal !</h4>
 
-      
+      <label htmlFor=""></label>
 
-        < form border-succes action="">
+      <textarea
 
-            <label htmlFor=""></label>
+        name=""
+        id=""
+        cols="50"
+        rows="5"
+        placeholder="Enter task..."
+        value={newTask}
+        onChange={handleInput} >  
+        
+        </textarea>
 
-            <textarea name="" id="" cols="50" rows="5" placeholder="Write your task !">
+      <br />
 
-            </textarea>
+      <button
+        className="bg-danger-subtle border-warning border-opacity-75 "
+        type="submit"
+        onClick={addTask} >
+          
+        Add new task </button>
 
-                <br />
-                
-            <button className="bg-danger-subtle border-warning border-opacity-75 " type="submit">Add new task </button>
-            
-            </form>
+      <ol>
+        {task.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)} className='btn btn-outline-danger'><img className='iconssize' src="src/assets/bin.svg" alt="deletebtn" />  </button>
+          </li>
+        ))}
+      </ol>
 
     </div>
-     
-    )
+  );
 }
 
-export default AddnewTask; 
+export default AddnewTask;
